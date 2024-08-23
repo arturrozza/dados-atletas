3c8ae35d23938085e03f98d00e54a98f41c44720
arturrozza <155444514+arturrozza@users.noreply.github.com>
2024-08-23 09:25:39 -0300

    segundo commit

--- dados-atletas.js
+++ dados-atletas.js
@@ -6,4 +6,24 @@ class Atleta {
         this.altura = altura;
         this.notas = notas;
     }
+
+    calculaCategoria() {
+        if (this.idade >= 9 && this.idade <+ 11) {
+            return "Infantil";
+        } else if (this.idade >= 12 && this.idade <= 13) {
+            return "Juvenil";
+        } else if (this.idade >= 14 && this.idade <= 15) {
+            return "Intermediário";
+        } else if (this.idade >= 16 && this.idade <= 30) {
+            return "Adulto";
+        } else {
+            return "Sem categoria";
+        }
+    }
+
+    calculaIMC() {
+        return this.peso / (this.altura * this.altura)
+    }
+
+
 }
